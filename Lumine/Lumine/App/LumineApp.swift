c75bcec1261211107856a40aef1c6b6448aa521e
youn9k <ace_lephant@naver.com>
2025-11-24 13:11:02 +0900

    이전 윈도우 크기 기억

--- Lumine/Lumine/App/LumineApp.swift
+++ Lumine/Lumine/App/LumineApp.swift
@@ -7,9 +7,23 @@ struct LumineApp: App {
     }
     
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "MainWindow") {
             MainView()
         }
         .windowToolbarLabelStyle(fixed: .automatic)
+        #if os(macOS)
+        .defaultSize(calculateDefaultWindowSize())
+        #endif
     }
+    
+    #if os(macOS)
+    private func calculateDefaultWindowSize() -> CGSize {
+        if let screen = NSScreen.main {
+            let width = screen.visibleFrame.width * (2.0 / 3.0)
+            let height = width * (9.0 / 16.0)
+            return CGSize(width: width, height: height)
+        }
+        return CGSize(width: 1200, height: 675) // Fallback 16:9
+    }
+    #endif
 }
